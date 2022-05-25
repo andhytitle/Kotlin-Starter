3f7d298b278dd4a5b25d99858d6a915ab9b6a5df
andhytitle <andikarusli27@gmail.com>
2022-05-25 09:33:49 +0700

    Kotlin Starter

---
+++ src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -0,0 +1,33 @@
+package com.dicoding.kotlin
+
+// main function
+fun main() {
+
+    //Kotlin Fundamental/Pengenalan
+//    val name = "Alfian"
+//    print("Hello my name is ")
+//    println(name)
+//    print(if (true)"Always true" else "Always false")
+
+
+    //Penggunaan Operator +
+//    val firstWord = "Dicoding "
+//    val lastWord = "Academy"
+//    print(firstWord + lastWord)
+
+    //Penggunaan Operator + pada operator bertipe Int
+//    val valueA: Int = 10
+//    val valueB = 20
+//    print(valueA + valueB)
+
+    // Penggunaan tipe char('') menggunakan tanda petik satu dan satu karakter
+//    var vocal = '1'
+//    println("Vocal " + vocal++)
+//    println("Vocal " + vocal++)
+//    println("Vocal " + vocal++)
+//    println("Vocal " + vocal--)
+//    println("Vocal " + vocal--)
+//    println("Vocal " + vocal--)
+//    println("Vocal " + vocal--)
+
+}
